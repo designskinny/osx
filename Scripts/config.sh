f5a315a096a1c107cbf34214ac18c77d0c1e68d9
designskinny <31420996+designskinny@users.noreply.github.com>
2020-04-26 18:05:45 -0700

    Update config.sh

--- Scripts/config.sh
+++ Scripts/config.sh
@@ -10,3 +10,29 @@ echo ""
 echo "[CONFIG] Enable developer mode for admin users"
 sudo /usr/sbin/DevToolsSecurity -enable
 echo ""
+
+echo "[CONFIG] Disable 'Open application from the web' confirmation prompt"
+defaults write com.apple.LaunchServices LSQuarantine -bool false
+echo ""
+
+echo "[CONFIG] Disable Time Machine prompt for external disks"
+defaults write com.apple.TimeMachine DoNotOfferNewDisksForBackup -bool true
+echo ""
+
+echo "[CONFIG] Default to expanded Print dialogs"
+defaults write NSGlobalDomain PMPrintingExpandedStateForPrint -bool true
+echo ""
+
+echo "[CONFIG] Default to expanded Save panels"
+defaults write NSGlobalDomain NSNavPanelExpandedStateForSaveMode -bool true
+echo ""
+
+echo "[CONFIG] Default to using tabs instead of windows"
+defaults write NSGlobalDomain AppleWindowTabbingMode -string always
+echo ""
+
+echo "[CONFIG]"
+
+echo ""
+
+
